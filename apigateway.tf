e036ea2f49e25b42a4c6263048ac6667c471d020
akram.dhiabi <akram.dhiabi@filestage.io>
2022-01-24 22:42:46 +0100

    refactor and add api resource

--- apigateway.tf
+++ apigateway.tf
@@ -1,37 +1,11 @@
-resource "aws_apigatewayv2_api" "workmotion_api" {
-  name          = "workmotion_api"
-  protocol_type = "HTTP"
+resource "aws_api_gateway_rest_api" "workmotion_apigw" {
+  name          = "workmotion_apigw"
 }
 
 resource "aws_cloudwatch_log_group" "workmotion_api" {
-  name = "/aws/api_gw/${aws_apigatewayv2_api.workmotion_api.name}"
+  name = "/aws/api_gw/${aws_api_gateway_rest_api.workmotion_apigw.name}"
 
   retention_in_days = 30
-}
-
-resource "aws_apigatewayv2_stage" "lambda" {
-  api_id = aws_apigatewayv2_api.workmotion_api.id
-
-  name        = "workmotion_lambda_stage"
-  auto_deploy = true
-
-  access_log_settings {
-    destination_arn = aws_cloudwatch_log_group.workmotion_api.arn
-
-    format = jsonencode({
-      requestId               = "$context.requestId"
-      sourceIp                = "$context.identity.sourceIp"
-      requestTime             = "$context.requestTime"
-      protocol                = "$context.protocol"
-      httpMethod              = "$context.httpMethod"
-      resourcePath            = "$context.resourcePath"
-      routeKey                = "$context.routeKey"
-      status                  = "$context.status"
-      responseLength          = "$context.responseLength"
-      integrationErrorMessage = "$context.integrationErrorMessage"
-      }
-    )
-  }
 }
 
 resource "aws_lambda_permission" "apigw_workmotion_lambda" {
@@ -40,5 +14,11 @@ resource "aws_lambda_permission" "apigw_workmotion_lambda" {
   function_name = aws_lambda_function.workmotion_lambda.function_name
   principal     = "apigateway.amazonaws.com"
 
-  source_arn = "${aws_apigatewayv2_api.workmotion_api.execution_arn}/*/*"
+  source_arn = "${aws_api_gateway_rest_api.workmotion_apigw.execution_arn}/*/*"
+}
+
+resource "aws_api_gateway_resource" "workmotion_api" {
+  parent_id   = aws_api_gateway_rest_api.workmotion_apigw.root_resource_id
+  path_part   = "api"
+  rest_api_id = aws_api_gateway_rest_api.workmotion_api.id
 }
