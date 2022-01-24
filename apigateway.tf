ad9e8dd5eeee8bbb77ad2422e5e5c8b81a282168
akram.dhiabi <akram.dhiabi@filestage.io>
2022-01-24 22:13:03 +0100

    add iam permissions to api gateway to run Lambda

--- apigateway.tf
+++ apigateway.tf
@@ -33,3 +33,12 @@ resource "aws_apigatewayv2_stage" "lambda" {
     )
   }
 }
+
+resource "aws_lambda_permission" "api_gw" {
+  statement_id  = "AllowExecutionFromAPIGateway"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.workmotion_lambda.function_name
+  principal     = "apigateway.amazonaws.com"
+
+  source_arn = "${aws_apigatewayv2_api.workmotion_api.execution_arn}/*/*"
+}
