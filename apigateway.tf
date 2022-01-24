1a559ea2773541e0a93911d3fba73e92edfc06bd
akram.dhiabi <akram.dhiabi@filestage.io>
2022-01-25 00:27:02 +0100

    add post method integration

--- apigateway.tf
+++ apigateway.tf
@@ -23,6 +23,7 @@ resource "aws_api_gateway_resource" "workmotion_api" {
   rest_api_id = aws_api_gateway_rest_api.workmotion_apigw.id
 }
 
+# GET method integration
 resource "aws_api_gateway_method" "workmotion_get" {
   authorization = "NONE"
   http_method   = "GET"
@@ -34,6 +35,27 @@ resource "aws_api_gateway_integration" "workmotion_get" {
   rest_api_id             = aws_api_gateway_rest_api.workmotion_apigw.id
   resource_id             = aws_api_gateway_resource.workmotion_api.id
   http_method             = aws_api_gateway_method.workmotion_get.http_method
+  integration_http_method = "POST"
+  type                    = "AWS"
+  passthrough_behavior    = "WHEN_NO_TEMPLATES"
+  uri                     = aws_lambda_function.workmotion_lambda.invoke_arn
+  request_templates = { # Not documented
+    "application/json" = "${file("./lambda-payload/workmotion_body_mapping.tpl")}"
+  }
+}
+
+# POST method integration
+resource "aws_api_gateway_method" "workmotion_post" {
+  authorization = "NONE"
+  http_method   = "POST"
+  resource_id   = aws_api_gateway_resource.workmotion_api.id
+  rest_api_id   = aws_api_gateway_rest_api.workmotion_apigw.id
+}
+
+resource "aws_api_gateway_integration" "workmotion_post" {
+  rest_api_id             = aws_api_gateway_rest_api.workmotion_apigw.id
+  resource_id             = aws_api_gateway_resource.workmotion_api.id
+  http_method             = aws_api_gateway_method.workmotion_post.http_method
   integration_http_method = "POST"
   type                    = "AWS"
   passthrough_behavior    = "WHEN_NO_TEMPLATES"
