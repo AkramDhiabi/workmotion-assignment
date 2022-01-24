c36c74a998b1a5603d2f35f8df6d6ac5f1be106b
akram.dhiabi <akram.dhiabi@filestage.io>
2022-01-24 23:21:55 +0100

    add api gateway deployment dev

--- apigateway.tf
+++ apigateway.tf
@@ -41,3 +41,11 @@ resource "aws_api_gateway_integration" "workmotion_get" {
     "application/json" = "${file("./lambda-payload/workmotion_body_mapping.tpl")}"
   }
 }
+
+resource "aws_api_gateway_deployment" "workmotion_dev" {
+  depends_on = [
+    aws_api_gateway_integration.workmotion_get
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.workmotion_apigw.id
+}
