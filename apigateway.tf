5ee3536e8bb93246a6b68e608b14176cbadc4ad9
akram.dhiabi <akram.dhiabi@filestage.io>
2022-01-24 22:10:25 +0100

    add api gateway stage

--- apigateway.tf
+++ apigateway.tf
@@ -2,3 +2,28 @@ resource "aws_apigatewayv2_api" "workmotion_api" {
   name          = "workmotion_api"
   protocol_type = "HTTP"
 }
+
+resource "aws_apigatewayv2_stage" "lambda" {
+  api_id = aws_apigatewayv2_api.workmotion_api.id
+
+  name        = "workmotion_lambda_stage"
+  auto_deploy = true
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.workmotion_api.arn
+
+    format = jsonencode({
+      requestId               = "$context.requestId"
+      sourceIp                = "$context.identity.sourceIp"
+      requestTime             = "$context.requestTime"
+      protocol                = "$context.protocol"
+      httpMethod              = "$context.httpMethod"
+      resourcePath            = "$context.resourcePath"
+      routeKey                = "$context.routeKey"
+      status                  = "$context.status"
+      responseLength          = "$context.responseLength"
+      integrationErrorMessage = "$context.integrationErrorMessage"
+      }
+    )
+  }
+}
