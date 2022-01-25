1d980f02582f8428e8bc5fe72bfa8099e366c371
akram.dhiabi <akram.dhiabi@filestage.io>
2022-01-25 13:34:13 +0100

    output full invoke url path

--- outputs.tf
+++ outputs.tf
@@ -5,5 +5,5 @@ output "function_name" {
 
 output "deployment_invoke_url" {
   description = "Deployment invoke url"
-  value       = aws_api_gateway_deployment.workmotion_deployment.invoke_url
+  value       = "${aws_api_gateway_stage.workmotion_stage.invoke_url}/${trimprefix("${aws_api_gateway_resource.workmotion_api.path}", "/")}"
 }
