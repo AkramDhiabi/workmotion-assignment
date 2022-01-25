31a42a1963a1ea922ebe7347d35fcabbf3138a0b
akram.dhiabi <akram.dhiabi@filestage.io>
2022-01-25 12:01:24 +0100

    new line

--- lambda-payload/index.js
+++ lambda-payload/index.js
@@ -2,7 +2,8 @@ exports.handler = function(event, context, callback) {
   const outputmsg = `Welcome to our demo API, here are the details of your request:
   Headers: ${JSON.stringify(event.headers)}
   Method: ${JSON.stringify(event.method)}
-  Body: ${JSON.stringify(event.body)}`;
+  Body: ${JSON.stringify(event.body)}
+  `;
   
   callback(null, outputmsg);
 };
