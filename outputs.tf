2985c71eeb6a673a9335480d8c0da9e494e200a7
akram.dhiabi <akram.dhiabi@filestage.io>
2022-01-25 13:10:07 +0100

    add deployment invoke url

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "function_name" {
   description = "Name of the Lambda function."
-  value = aws_lambda_function.workmotion_lambda.function_name
+  value       = aws_lambda_function.workmotion_lambda.function_name
 }
 
-output "base_url" {
-  description = "Base URL for API Gateway stage."
-  value = aws_api_gateway_rest_api.workmotion_apigw.invoke_url
+output "deployment_invoke_url" {
+  description = "Deployment invoke url"
+  value       = aws_api_gateway_deployment.workmotion_dev.invoke_url
 }
